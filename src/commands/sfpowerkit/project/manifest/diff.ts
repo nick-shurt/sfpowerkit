012c45dad1f1758479ddfc92fcf576891d9e1273
nick-shurt <47427008+nick-shurt@users.noreply.github.com>
2020-10-14 12:10:22 -0400

    Update diff.ts

--- src/commands/sfpowerkit/project/manifest/diff.ts
+++ src/commands/sfpowerkit/project/manifest/diff.ts
@@ -12,97 +12,75 @@ core.Messages.importMessagesDirectory(__dirname);
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
 const messages = core.Messages.loadMessages(
-  "sfpowerkit",
-  "project_manifest_diff"
+	"sfpowerkit",
+	"project_manifest_diff"
 );
 
 export default class Diff extends SfdxCommand {
-  public static description = messages.getMessage("commandDescription");
+	public static description = messages.getMessage("commandDescription");
 
-  public static examples = [
-    `$ sfdx sfpowerkit:project:manifest:diff -s source/package.xml -t target/package.xml -d output`
-  ];
+	public static examples = [
+		`$ sfdx sfpowerkit:project:manifest:diff -s source/package.xml -t target/package.xml -d output`
+	];
 
-  protected static flagsConfig = {
-    loglevel: flags.enum({
-      description: messages.getMessage("loglevel"),
-      default: "info",
-      required: false,
-      options: [
-        "trace",
-        "debug",
-        "info",
-        "warn",
-        "error",
-        "fatal",
-        "TRACE",
-        "DEBUG",
-        "INFO",
-        "WARN",
-        "ERROR",
-        "FATAL"
-      ]
-    })
-  };
+  	protected static flagsConfig = {
+    	loglevel: flags.enum({
+    		description: messages.getMessage("loglevel"),
+			default: "info",
+			required: false,
+			options: [
+			"trace",
+			"debug",
+			"info",
+			"warn",
+			"error",
+			"fatal",
+			"TRACE",
+			"DEBUG",
+			"INFO",
+			"WARN",
+			"ERROR",
+			"FATAL"
+			]
+    	})
+  	};
 
-  public async run(): Promise<AnyJson> {
-    var fs = require("fs"),
-    xml2js = require("xml2js");
+	public async run(): Promise<AnyJson> {
+		var fs = require("fs"),
+		xml2js = require("xml2js");
 
-    var parser = new xml2js.Parser();
-    fs.readFile("./package/package.xml", function(err, data) {
-      if (err) {
-        throw err;
-      }
+		fs.readFile("./package/package.xml", function(err, data) {
+			if (err) { throw err; }
 
-      xml2js.parseString(data, (err, result) => {
-        if (err) {
-          throw err;
-        }
+			xml2js.parseString(data, (err, result) => {
+				if (err) { throw err; }
+				var objArray = [];
 
-        const labels = {
-          members: '*',
-          name: 'CustomLabels'
-        };
+				result.Package.types.forEach(type => {
+					if (type.name == "CustomLabel") {
+						result.Package.types.push({members: '*', name: 'CustomLabels'});
+					}
+					if (type.name == "CustomField") {
+						type.members.array.forEach(member => {
+							var sObject = member.substr(0, member.indexOf('.'));
+							objArray.push(sObject);
+						});
+					}
+				});
 
-        result.Package.types.push(labels);
-        
-        // convert SJON objec to XML
-        const builder = new xml2js.Builder();
-        const xml = builder.buildObject(result);
+				if (Array.isArray(objArray) && objArray.length) {
+					result.Package.types.push({members: objArray[0], name: 'CustomObject'});
+				}
 
-        // write updated XML string to a file
-        fs.writeFile('./package/package.xml', xml, (err) => {
-            if (err) {
-                throw err;
-            }
+				const builder = new xml2js.Builder();
+				const xml = builder.buildObject(result);
 
-            console.log(`Updated XML is written to a new file.`);
-        });
-      });
-
-      /*parser.parseString(data, function(err, result) {
-        result.Package.types.forEach(element => {
-          if (element.name == "ApexClass") {
-            element.members.forEach(cls => {
-              if (
-                cls.startsWith("Test") ||
-                cls.startsWith("test") ||
-                cls.endsWith("Test") || cls.endsWith("test")
-              ) {
-                fs.appendFile("./testsToRun/testFile.txt", cls + ",", function(
-                  err
-                ) {
-                  if (err) throw err;
-                });
-                console.log(cls);
-              }
-            });
-          }
-        });
-      });*/
-    });
-
-    return null;
-  }
+				fs.writeFile('./package/package.xml', xml, (err) => {
+					if (err) { throw err; }
+					console.log(`Updated XML is written to a new file.`);
+				});
+			});
+		});
+		return null;
+	}
 }
